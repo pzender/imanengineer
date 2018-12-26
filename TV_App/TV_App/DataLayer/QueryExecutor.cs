c306b261cdcda788c653dec5645a2f895fb7eded
pzender <ememek96@gmail.com>
2018-12-26 20:42:46 +0100

    recreated ClientApp with new Angular, fixed db connection

--- TV_App/TV_App/DataLayer/QueryExecutor.cs
+++ TV_App/TV_App/DataLayer/QueryExecutor.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SQLite;
 using Dapper;
+using System.IO;
 
 namespace DataLayer
 {
     public class QueryExecutor
     {
-        private readonly string ConnString = @"Data Source=C:\Users\Przemek\source\repos\TV_App\TV_App\DataLayer\test.sqlite;Version=3;";
+        const string PROJECT_DIRECTORY_NAME = "TV_App";
+        const string DATABASE_FILE = "test.sqlite";
+
+        private readonly string ConnString = $"Data Source={SQLiteFileLocation()};Version=3;";
         private SQLiteConnection Conn { get { return new SQLiteConnection(ConnString); } }
 
         public IEnumerable<T> Query<T>(string query, IDictionary<string, object> parameters = null)
@@ -17,7 +21,20 @@ namespace DataLayer
             
             return Conn.Query<T>(query);
         }
-        
 
+
+
+
+        private static string SQLiteFileLocation()
+        {
+            string path = Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory);
+            if (path.Contains(PROJECT_DIRECTORY_NAME))
+            {
+                path = Path.Combine(path.Substring(0, path.LastIndexOf(PROJECT_DIRECTORY_NAME)), PROJECT_DIRECTORY_NAME, "DataLayer", DATABASE_FILE);
+                return path;
+            }
+            else throw new DirectoryNotFoundException($"directory {PROJECT_DIRECTORY_NAME} not found in path {path}");
+
+        }
     }
 }
