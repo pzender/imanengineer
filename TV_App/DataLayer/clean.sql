4ae3a027c57fce8c5622c4724076f13d9b361913
pzender <ememek96@gmail.com>
2019-01-15 14:48:58 +0100

    db clean script

---
+++ TV_App/DataLayer/clean.sql
@@ -0,0 +1,8 @@
+delete from FeatureExample;
+delete from Feature;
+delete from Emission;
+delete from Description;
+delete from Rating;
+delete from Programme;
+delete from Channel;
+delete from GuideUpdate;
