9331a43f1a59e126b13a968367c2ad5c89a32fe2
Przemek <pzender@hotmail.com>
2019-08-25 11:05:28 +0200

    rate button now displays rating info

--- client/src/app/shared/components/listing-element/listing-element.component.ts
+++ client/src/app/shared/components/listing-element/listing-element.component.ts
@@ -38,10 +38,13 @@ export class ListingElementComponent implements OnInit {
   rateButton(value: number) {
     this._http.post(
       `${environment.api}Users/${this.user.getUser()}/Ratings`,
-      {ProgrammeId: this.programme.id, RatingValue: value},
-      {responseType: 'json'}
+      { ProgrammeId: this.programme.id, RatingValue: value },
+      { responseType: 'json' }
     )
-    .subscribe(result => this.buttonClicked.emit(result.toString()));
+    .subscribe(result => {
+      this.buttonClicked.emit(result.toString());
+      this.programme.rating = result['RatingValue']
+    });
   }
 
   remindme() {
