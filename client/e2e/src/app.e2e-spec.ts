a73dfe462bdd8b6121ddecc359ed6f1bc5265338
Przemek <pzender@hotmail.com>
2019-12-01 00:11:55 +0100

    e2e tests + description

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -5,10 +5,32 @@ describe('workspace-project App', () => {
 
   beforeEach(() => {
     page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ClientApp!');
+  it('should display app name', () => {
+    expect(page.getNavbarTitle()).toEqual('Looking Glass');
+  });
+
+  it('should display channels at main page', () => {
+    expect(page.getHeader()).toEqual('156 kanałów do wyboru');
+  });
+
+  it('should display listing at channel page', () => {
+    let link = page.getChannelLink();
+    expect(link).toBeTruthy();
+    link.click();
+    expect(page.getHeader()).toEqual('TVP 1');
+    page.nextDay();
+    expect(page.getHeader()).toEqual('TVP 1');
+  });
+
+  it('should display details at programme page', () => {
+    page.getChannelLink().click();
+    page.nextDay();
+    let prog_link = page.getFirstTitle();
+    let prog_link_text = prog_link.getText();
+    prog_link.click();
+    expect(page.getHeader()).toEqual(prog_link_text);
   });
 });
--- client/e2e/src/app.po.ts
+++ client/e2e/src/app.po.ts
@@ -5,7 +5,23 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getHeader() {
+    return element(by.css('h1')).getText();
+  }
+
+  getNavbarTitle() {
+    return element(by.css('app-navbar nav a')).getText();
+  }
+
+  getChannelLink() {
+    return element(by.cssContainingText('a.text-light', 'TVP 1'));
+  }
+
+  nextDay() {
+    element(by.cssContainingText('#day a', '>')).click();
+  }
+
+  getFirstTitle() {
+    return element(by.css('app-listing-element h5 a'));
   }
 }
